f3cb39c79f8987c5743339bf855fac11bb42e5a1
Levi Gonzales <lvstross@gmail.com>
2017-10-25 15:33:34 -0700

    Added the layout template as well as the styles for the pdf blade templates

--- resources/views/layouts/pdf.blade.php
+++ resources/views/layouts/pdf.blade.php
@@ -15,20 +15,35 @@
     .clear-fix { clear: both; }
     .clear-left { clear: left; }
     .clear-right { clear: right; }
+    .inline { display: inline-block; }
     /* spacing */
     .padding-5 { padding: 5px; }
+    .padding-15 { padding: 15px; }
     .padding-50 { padding: 50px; }
+    .padding-20-sides { padding-left: 20px; padding-right: 20px; }
+    .padding-25-sides { padding-left: 25px; padding-right: 25px; }
+    .padding-46-sides { padding-left: 46px; padding-right: 46px; }
+    .margin-bottom-20 { margin-bottom: 20px; }
+    .margin-little { margin: 5px; }
+    .margin-none { margin: 0; }
     .space { height: 10px }
+    .more-space { height: 90px; }
+    .height-150 { max-height: 150px; }
+    .small-width { width: 10%; }
+    .qtr-width { width: 25%; }
     .half-width { width: 50%; }
     .full-width { width: 100%; }
-    /*.under-half { width: 40%; }*/
-    .qtr-width { width: 33.5%; }
-    .set-height { height: 100px; }
+    .under-half { width: 40%; }
+    .qtr-width { width: 29%; }
+    .set-height { height: 50px; }
     /* decoration */
     .border { border: 1px solid #000; }
+    .light-border { border: 1px solid #aaa; }
+    .underline { text-decoration: underline; }
     /* text & font */
     .font { font-family: Helvetica, Georgia, serif }
     .text-center { text-align: center; }
+    .text-right { text-align: right; }
     .sm-text { font-size: 12px; }
     .md-text { font-size: 14px; }
     .lg-text { font-size: 16px; }

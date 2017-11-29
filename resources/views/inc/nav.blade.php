7cc8e6eedbd9167a38a1ce1825e4e9ecef8c56ec
Levi Gonzales <lvstross@gmail.com>
2017-11-29 12:53:03 -0800

    optimized the user permissions in the nav bar. Removed the use of the User model in the template

--- resources/views/inc/nav.blade.php
+++ resources/views/inc/nav.blade.php
@@ -1,4 +1,3 @@
-<?php use App\User; ?>
         <!-- Navigation -->
         <nav class="navbar navbar-inverse main-color navbar-fixed-top" role="navigation">
             <!-- Brand and toggle get grouped for better mobile display -->
@@ -16,11 +15,13 @@
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> {{ Auth::user()->name }} <b class="caret"></b></a>
                     <ul class="dropdown-menu">
-                        <li class="padding">
-                            <a href="{{ url('/settings') }}"><i class="fa fa-fw fa-gear"></i> Settings</a>
-                        </li>
-                        <li class="divider"></li>
-                        <li class="padding">
+                        @if( Auth::user()->permission == 1 )
+                            <li class="padding" style="margin-right: 10px">
+                                <a href="{{ url('/settings') }}"><i class="fa fa-fw fa-gear"></i> Settings</a>
+                            </li>
+                            <li class="divider"></li>
+                        @endif
+                        <li class="padding" style="margin-right: 10px;">
                             <a href="{{ route('logout') }}" 
                                 onclick="event.preventDefault();
                                         document.getElementById('logout-form').submit();">
@@ -41,22 +42,22 @@
                     <li>
                         <a href="{{ url('home') }}"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
                     </li>
-                    @if( User::isSuperAdmin() || User::isAdmin() )
+                    @if( Auth::user()->permission == 1 || Auth::user()->permission == 2 )
                         <li><a href="{{ url('invoices') }} "><i class="fa fa-fw fa-money"></i> Invoices</a></li>
                     @endif
                     <li>
                         <a href="javascript:;" data-toggle="collapse" data-target="#assets"><i class="fa fa-fw fa-database"></i> Assets <i class="fa fa-fw fa-caret-down"></i></a>
                         <ul id="assets" class="collapse">
 
-                            @if( User::isSuperAdmin() )
+                            @if( Auth::user()->permission == 1 )
                                 <li><a href="{{ url('users') }}"><i class="fa fa-fw fa-user"></i> Users</a></li>
                             @endif
 
-                            @if( User::isSuperAdmin() || User::isAdmin() || User::isManager() )
+                            @if( Auth::user()->permission == 1 || Auth::user()->permission == 2 || Auth::user()->permission == 3 )
                                 <li><a href="{{ url('products') }}"><i class="fa fa-fw fa-plane"></i> Products</a></li>
                             @endif
 
-                            @if( User::isSuperAdmin() || User::isAdmin() )
+                            @if( Auth::user()->permission == 1 || Auth::user()->permission == 2 )
                                 <li><a href="{{ url('customers') }}"><i class="fa fa-fw fa-users"></i> Customers</a></li>
                             @endif
 

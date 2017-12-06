994c7a43a78a530adb3ea50290d12dbe402da104
Levi Gonzales <lvstross@gmail.com>
2017-12-06 08:58:44 -0800

    Added a method for double decoding double encoded values

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,14 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+    * Double Decode Decoded JSON Data For PDF Class
+    *
+    * @param $val  Double Encoded String
+    * @return Double Decoded String
+    */
+    public function doubleDecode($val){
+        return json_decode(json_decode($val, true));
+    }
 }

6eec932cc3992dba9384cd5fd578e7953ceb61f4
Levi Gonzales <lvstross@gmail.com>
2018-01-02 12:39:38 -0800

    Tried to add testing and made small layout changes

--- resources/views/inc/nav.blade.php
+++ resources/views/inc/nav.blade.php
@@ -8,7 +8,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="{{ url('/dashboard') }}">Data Entry</a>
+                <a class="navbar-brand" href="{{ url('/dashboard') }}">Dataentry</a>
             </div>
             <!-- Top Menu Items -->
             <ul class="nav navbar-right top-nav">

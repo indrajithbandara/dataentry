8791e0014bd4ca4908f752db2e3c369541d3f069
Levi Gonzales <lvstross@gmail.com>
2018-01-04 08:16:48 -0800

    experimented with mailgun driver

--- config/mail.php
+++ config/mail.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'driver' => env('MAIL_DRIVER', 'smtp'),
+    'driver' => env('MAIL_DRIVER', 'mailgun'),
 
     /*
     |--------------------------------------------------------------------------
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'Server Email'),
-        'name' => env('MAIL_FROM_NAME', 'Data Entry App'),
+        'address' => env('MAIL_FROM_ADDRESS', 'some address'),
+        'name' => env('MAIL_FROM_NAME', 'Dataentry System'),
     ],
 
     /*
--- config/services.php
+++ config/services.php
@@ -15,8 +15,8 @@ return [
     */
 
     'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
+        'domain' => env('MAIL_GUN_DOMAIN'),
+        'secret' => env('MAIL_GUN_KEY'),
     ],
 
     'ses' => [

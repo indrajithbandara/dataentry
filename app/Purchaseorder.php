115a5cfcf2328d6397defcdeda2a594d3008b3f3
Levi Gonzales <lvstross@gmail.com>
2018-01-11 10:25:17 -0800

    Added structure for a purchaseorders model, controller, vue file and migration.

---
+++ app/Purchaseorder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Purchaseorder extends Model
+{
+    protected $fillable = [
+        'po_num',
+        'due_date',
+        'will_ship',
+        'rating',
+        'sooner',
+        'customer',
+        'part_num',
+        'qty',
+        'status',
+        'notes'
+    ];
+
+    public $timestamps = false;
+}
---
+++ database/migrations/2018_01_11_162243_create_purchaseorders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePurchaseordersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchaseorders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('po_num', 50);
+            $table->date('due_date');
+            $table->date('will_ship');
+            $table->integer('rating');
+            $table->enum('sooner', ['Yes', 'No']);
+            $table->string('customer', 100);
+            $table->string('part_num', 50);
+            $table->integer('qty');
+            $table->enum('status', ['Open', 'Closed']);
+            $table->text('notes')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchaseorders');
+    }
+}

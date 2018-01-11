3f887905d155ed6e8adf47e794eed9691556dc23
Levi Gonzales <lvstross@gmail.com>
2018-01-11 10:29:40 -0800

    Added components for a departments vue file, a routers vue file, a purchase orders vue file and an inventory vue file.

--- resources/assets/js/modules/vue-module.js
+++ resources/assets/js/modules/vue-module.js
@@ -10,6 +10,10 @@ Vue.component('users', require('./../components/users.vue'));
 Vue.component('customers', require('./../components/customers.vue'));
 Vue.component('products', require('./../components/products.vue'));
 Vue.component('invoices', require('./../components/invoices.vue'));
+Vue.component('routers', require('./../components/routers.vue'));
+Vue.component('departments', require('./../components/departments.vue'));
+Vue.component('inventory', require('./../components/inventory.vue'));
+Vue.component('purchaseorders', require('./../components/purchaseorders.vue'));
 
 // Vue instances when element is present
 if (document.getElementById('dashboard-app')) {
@@ -24,4 +28,12 @@ if (document.getElementById('dashboard-app')) {
     const prodApp = new Vue({ el: '#products-app', store: store });
 } else if (document.getElementById('invoice-app')) {
     const invApp = new Vue({ el: '#invoice-app', store: store });
-}
+} else if (document.getElementById('routers-app')) {
+    const routerApp = new Vue({ el: '#routers-app', store: store });
+} else if (document.getElementById('departments-app')) {
+    const deptApp = new Vue({ el: '#departments-app', store: store });
+} else if (document.getElementById('inventory-app')) {
+    const inventoryApp = new Vue({ el: '#inventory-app', store: store });
+} else if (document.getElementById('purchaseorders-app')) {
+    const poApp = new Vue({ el: '#purchaseorders-app', store: store });
+} 

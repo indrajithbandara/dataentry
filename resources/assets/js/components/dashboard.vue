2ef71460b039e3e5b500beda170027fe621e6406
Levi Gonzales <lvstross@gmail.com>
2018-01-11 14:23:23 -0800

    Added other wells back to the page. Changed the customer well to an inventory well. Also changing this area from just counting the items to counting the ones that are either open, unapproved or incomplete

--- resources/assets/js/components/dashboard.vue
+++ resources/assets/js/components/dashboard.vue
@@ -1,16 +1,16 @@
 <template>
     <div>
         <div class="row">
-<!--             <div class="col-lg-3 col-md-6">
+            <div class="col-lg-3 col-md-6">
                 <div class="panel panel-info">
                     <div class="panel-heading">
                         <div class="row">
                             <div class="col-xs-3">
-                                <i class="fa fa-file-text-o fa-5x"></i>
+                                <i class="fa fa-tasks fa-5x"></i>
                             </div>
                             <div class="col-xs-9 text-right">
                                 <div class="huge">0</div>
-                                <div>Open PO's</div>
+                                <div>Open Purchase Orders</div>
                             </div>
                         </div>
                     </div>
@@ -22,8 +22,8 @@
                         </div>
                     </a>
                 </div>
-            </div> -->
-            <div class="col-xs-12 col-md-6">
+            </div>
+            <div class="col-lg-3 col-md-6">
                 <div class="panel panel-success">
                     <div class="panel-heading">
                         <div class="row">
@@ -32,7 +32,7 @@
                             </div>
                             <div class="col-xs-9 text-right">
                                 <div class="huge">{{ inv_count }}</div>
-                                <div>Invoices</div>
+                                <div>Incomplete Invoices</div>
                             </div>
                         </div>
                     </div>
@@ -45,7 +45,7 @@
                     </a>
                 </div>
             </div>
-<!--             <div class="col-lg-3 col-md-6">
+            <div class="col-lg-3 col-md-6">
                 <div class="panel panel-warning">
                     <div class="panel-heading">
                         <div class="row">
@@ -66,17 +66,17 @@
                         </div>
                     </a>
                 </div>
-            </div> -->
-            <div class="col-xs-12 col-md-6">
+            </div>
+            <div class="col-lg-3 col-md-6">
                 <div class="panel panel-danger">
                     <div class="panel-heading">
                         <div class="row">
                             <div class="col-xs-3">
-                                <i class="fa fa-users fa-5x"></i>
+                                <i class="fa fa-ticket fa-5x"></i>
                             </div>
                             <div class="col-xs-9 text-right">
-                                <div class="huge">{{ cust_count }}</div>
-                                <div>Customers</div>
+                                <div class="huge">{{ inventory_count }}</div>
+                                <div>Unapproved Inventory Ship Tickets</div>
                             </div>
                         </div>
                     </div>
@@ -99,12 +99,13 @@
     export default {
         beforeMount(){
             this.invoiceCount();
-            this.customerCount();
         },
         data() {
             return {
+                po_count: 0,
                 inv_count: 0,
-                cust_count: 0
+                router_count: 0,
+                inventory_count: 0
             }
         },
         methods: {
@@ -117,15 +118,33 @@
                     throw new Error("Something went wrong getting your Invoice count.");
                 });
             },
-            customerCount(){
-                axios.get('api/customers/count')
-                .then((response)=>{
-                    this.cust_count = response.data;
-                })
-                .catch((error)=>{
-                    throw new Error("Something went wrong getting your Customer count.");
-                });
-            }
+            // purchaseorderCount(){
+            //     axios.get('api/purchaseorders/count')
+            //     .then((response)=>{
+            //         this.po_count = response.data;
+            //     })
+            //     .catch((error)=>{
+            //         throw new Error("Something went wrong getting your purchaseorder count.");
+            //     });
+            // },
+            // routerCount(){
+            //     axios.get('api/routers/count')
+            //     .then((response)=>{
+            //         this.router_count = response.data;
+            //     })
+            //     .catch((error)=>{
+            //         throw new Error("Something went wrong getting your router count.");
+            //     });
+            // },
+            // inventoryCount(){
+            //     axios.get('api/inventory/count')
+            //     .then((response)=>{
+            //         this.inventory_count = response.data;
+            //     })
+            //     .catch((error)=>{
+            //         throw new Error("Something went wrong getting your inventory count.");
+            //     });
+            // }
         }
     }
 </script>
